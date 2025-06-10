42af3d3a18e2f3235e8d67c0615fe262cebdb857
shravya-05 <shravyarai0505@gmail.com>
2025-06-10 08:48:53 +0530

    test_scraper.py

--- test_scraper.py
+++ test_scraper.py
@@ -4,44 +4,52 @@ import pandas as pd
 import json
 from unittest.mock import patch
 
+
 class TestEmployeeDataScraper(unittest.TestCase):
     
     def setUp(self):
         self.filename = "employees.json"
 
+
     def test_01_json_file_exists(self):
-        """Test Case 1: Check if JSON file exists."""
+        """test Case 1: check if json file exists"""
         print("employees.json exists:", os.path.isfile("employees.json"))  #should return True
 
     def test_02_json_file_extraction(self):
-        """Test Case 2: Verify JSON file extraction."""
+        """test case 2:verify json file extraction"""
         try:
+           # pd.set_option('display.max_columns', None) 
             df = pd.read_json(self.filename)
             print("\nFirst 5 rows of the dataset:\n", df.head())  
             self.assertIsInstance(df, pd.DataFrame)
         except Exception as e:
-            self.fail(f"JSON extraction failed: {e}")
-
+            self.fail(f"json extraction failed: {e}")
+            
+            
+                   
     def test_03_file_extension(self):
-        """Test Case 3: Verify file type and data type."""
+        """test Case 3: verify fileType and datatype"""
         df = pd.read_json("employees.json")
-        print("File extension:", os.path.splitext("employees.json")[1])  #should return '.json'
-        print("Age data type is", df["age"].dtype)  #should be 'INT'
-
+        print("File extension:", os.path.splitext("employees.json")[1])  # Should return '.json'
+        print(" Age data type is " ,df["age"].dtype)  #should be 'INT'
+        
+        
+                   
     def test_04_validate_data_structure(self):
-        """Test Case 4: Validate Data Structure."""
+        """Test Case 4: Validate Data Structure"""
         expected_keys = [
             "id", "first_name", "last_name", "email", "phone",
             "gender", "age", "job_title",
             "years_of_experience", "salary", "department"
         ]
         df = pd.read_json(self.filename)
-        print(df.columns.tolist() == expected_keys)  #should return True
-
+        print(df.columns.tolist()== expected_keys)  #should return True
+        
     def test_05_no_missing_values(self):
-        """Test Case 5: Handle Missing or Invalid Data."""
+        """Test Case 5: Handle Missing or Invalid Data"""
         df = pd.read_json(self.filename)
         print(df.isnull().sum().to_string())  #should show zero missing values    
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     unittest.main()
